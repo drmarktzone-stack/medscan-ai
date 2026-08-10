d398f3789aacb1e7e803dd6aef1089f654e26554
samear.abumukh <samear.abumukh@icloud.com>
2026-08-10 07:25:52 +0000

    External agent changes

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import BottomNav from "@/components/BottomNav";
+import PilotModeBanner from "@/components/PilotModeBanner";
 
 export default function AppLayout() {
   return (
     <>
+      <PilotModeBanner />
       <main className="pb-28">
         <Outlet />
       </main>
